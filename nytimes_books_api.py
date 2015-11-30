1687c27cecae597fa6aba69253738b2588cbc9d5
Mario Ynocente Castro <ycmario@gmail.com>
2015-11-30 10:07:02 +0100

    Add best_sellers_history

--- nytimes_books_api.py
+++ nytimes_books_api.py
@@ -8,4 +8,22 @@ def best_sellers(list_name):
     print "url :", url
     return json.load(urllib2.urlopen(url))
 
+def best_sellers_history(isbn=None, author=None, publisher=None, title=None):
+    url = "http://api.nytimes.com/svc/books/v3/lists/best-sellers/history?"
+    url += "api-key=" + books_api_key
+    if isbn is not None:
+        url += "&isbn=" + isbn
+    if publisher is not None:
+        url += "&publisher=" + publisher
+    if author is not None:
+        url += "&author=" + author
+    if title is not None:
+        url += "&title=" + title
+    print "url : ", url
+    return json.load(urllib2.urlopen(url))
+
 print json.dumps(best_sellers("manga"), indent=4, separators=(',', ': '))
+
+print json.dumps(best_sellers_history("9781421585659"), indent=4, separators=(',', ': '))
+
+print json.dumps(best_sellers_history(None, "ONE", "VIZ"), indent=4, separators=(',', ': '))
