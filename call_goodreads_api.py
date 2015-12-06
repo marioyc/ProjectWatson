9b98703eaa0ff59ca9252035173b27fa671cd384
BoeingX <song.baoyang@gmail.com>
2015-12-06 19:58:00 +0100

    add an example in goodreads_boks_api.py

--- call_goodreads_api.py
+++
@@ -1,6 +0,0 @@
-import json
-import goodreads_books_api
-number = '1'
-info = goodreads_books_api.get_information(number, 20)
-with open(str(number) + '.json', 'w') as outfile:
-    json.dump(info, outfile)
--- goodreads_books_api.py
+++ goodreads_books_api.py
@@ -12,6 +12,7 @@ print info
 import requests
 import pyisbn 
 import time
+import json
 import re
 from bs4 import BeautifulSoup
 
@@ -127,3 +128,9 @@ def remove_html_tags(string):
     using a regex
     """
     return re.sub('<[^[]+?>', '', string) 
+
+number = '1'
+info = get_information(number, 20)
+with open(str(number) + '.json', 'w') as outfile:
+        json.dump(info, outfile)
+
