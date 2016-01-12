9680e6f8f90fc98e6f6b1e9b5aa612808cb119ae
BoeingX <song.baoyang@gmail.com>
2016-01-12 20:07:02 +0100

    add benchmark

--- tf-idf.py
+++ tf-idf.py
@@ -13,9 +13,10 @@ from alchemyapi import AlchemyAPI
 
 alchemyapi = AlchemyAPI()
 
-def build_tf_idf(corpus, voc = None, preprocessed = True):
+def build_tf_idf(corpus, preprocessed = False, voc = None):
     if not preprocessed:
-        vectorizer = TfidfVectorizer(stop_words = 'english', lowercase = True, norm = 'l2')
+        #vectorizer = TfidfVectorizer(stop_words = 'english', lowercase = True, norm = 'l2')
+        vectorizer = TfidfVectorizer(norm = 'l2')
     else: 
         vectorizer = TfidfVectorizer(vocabulary = voc, norm = 'l2')
     return vectorizer.fit_transform(corpus)
@@ -31,15 +32,15 @@ def build_corpus(filenames):
     vocabulary = set(vocabulary)
     return reviews, vocabulary
 
-def get_review_keywords(filename, max_nb_reviews = 25):
+def get_review_keywords(filename, max_nb_reviews = 99):
     with open(filename) as infile:
         data = json.load(infile)
     reviews_raw = data.get('reviews')
-    if reviews_raw is None:
-        return 
+    if reviews_raw is None or len(reviews_raw) == 0:
+        return '', [] 
     reviews_raw = [i.get('body') for i in reviews_raw]
     nb_reviews = min(max_nb_reviews, len(reviews_raw))
-    reviews = ' '.join(reviews_raw[:nb_reviews])
+    reviews = '\n'.join(reviews_raw[:nb_reviews])
     response_entities = alchemyapi.entities("text", reviews)  
     entities = [i.get('text') for i in response_entities.get('entities')]
     response_keywords = alchemyapi.keywords("text", reviews)
@@ -54,12 +55,15 @@ def similarities(tf_idf):
    
 def main():
     if len(sys.argv) < 2:
-        filenames = ['data/1.json', 'data/120725.json', 'data/77366.json']
+        filenames = ['data/1.json', 'data/120725.json', 'data/77366.json', 'data/9520360.json', 'data/14406312.json', 'data/15872.json']
     else:
         filenames = sys.argv[1:]
     corpus, vocabulary = build_corpus(filenames)
-    tf_idf = build_tf_idf(corpus, vocabulary)
+    tf_idf = build_tf_idf(corpus, True, vocabulary)
+    tf_idf_benchmark = build_tf_idf(corpus)
     dist = similarities(tf_idf)
+    dist_benchmark = similarities(tf_idf_benchmark)
     print dist
+    print dist_benchmark
 
 main()
