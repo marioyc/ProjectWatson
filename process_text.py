d3791ec3a31616fca23db1e16beb152b5102610a
ana-mariacretu <cretu_ankutzyk@yahoo.com>
2016-01-31 21:58:58 +0100

    added text processing

---
+++ process_text.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 31 20:52:37 2016
+
+@author: Anca
+"""
+
+import re
+
+#Processes a keyword by: splitting it into unique words,
+#Removing the proper nouns (too particular to a given book),
+#And the punctuation except for the apostrophe
+def process_kw(kw):
+    keywords=kw.split()
+    l=[]
+    for word in keywords:
+        if ( word.islower() and (not "." in word)):
+            l.append(re.sub(ur"[^\w\d'\s]+",'',word))
+    return l
+    
+#Changing the text of a review by removing all punctuation 
+#except for the apostrophe and turning everything into lower cases
+def process_r(review):
+    return re.sub(ur"[^\w\d'\s]+",'',review).lower()
+    
+
+print process_r("Kewyword.s anglasids Harry's pottter*****, I really loved your wife")
