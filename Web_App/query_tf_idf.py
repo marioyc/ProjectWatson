0cc5309e8bf1db523db4658fd4d41dde7c4ca712
BoeingX <song.baoyang@gmail.com>
2016-02-12 14:42:23 +0100

    nouveau fichier : data/alchemy_tentative.txt 	nouveau fichier : data/dictionary.txt 	nouveau fichier : data/processed_books.txt 	modifié :         data/tf_idf.json 	nouveau fichier : data/vocabulary.txt 	renommé :         ../goodreads.py -> goodreads.py 	renommé :         ../matrix_tf_idf.py -> matrix_tf_idf.py 	renommé :         ../process_text.py -> process_text.py 	renommé :         ../query_tf_idf.py -> query_tf_idf.py 	renommé :         ../save_vocabulary.py -> save_vocabulary.py 	renommé :         ../tf_idf.py -> tf_idf.py 	supprimé :        ../alchemyapi.py

--- Web_App/query_tf_idf.py
+++ Web_App/query_tf_idf.py
@@ -2,40 +2,38 @@
 """
 Created on Thu Jan 21 14:39:16 2016
 
-@author: Anca
+@author: Ana-Maria,Baoyang
 """
 
+from tf_idf import *
 import string
 import os.path
+import numpy as np
 
-from numpy import array, inner
-from numpy.linalg import norm
-
-from tf_idf import *
-
-def cos(a, b):
-    if norm(a) == 0 or norm(b) == 0:
+def cos( a, b): 
+    if (np.linalg.norm(a)==0 or np.linalg.norm(b)==0):
         return 0
     else:
-        return round(inner(a, b)/(norm(a) * norm(b)), 3)
+        return round(np.inner(a, b)/(np.linalg.norm(a)*np.linalg.norm(b)), 3)
 
 #computes the similarity between the query and the corpus
 def match_query(path_json,query,top_n):
     #Fetching the ids that have already been processed
     processed_ids = set()
-    if os.path.isfile(path_json + 'processed.txt'):
-        f = open(path_json + 'processed.txt')
+    if os.path.isfile(path_json + 'alchemy_tentative.txt'):
+        f = open(path_json + 'alchemy_tentative.txt')
         for line in f:
             processed_ids.add(int(line.strip()))
         f.close()
     #Keeping only those ids that correspond to an existing .json file
     processed_ids=filter(lambda x: os.path.isfile(path_json+str(x)+'.json'), processed_ids)
+    print processed_ids
 
     #keeping only those filenames that exist
     filenames=[path_json+str(x)+'.json' for x in processed_ids]
 
     #Loading the description and the corpus of reviews
-    descriptions,reviews,_=build_corpus(filenames,False)
+    _, descriptions,reviews,_=build_corpus(filenames, extract_keywords = False)
 
     #Building the vectorizer for reviews
     vectorizer_r=build_tf_idf(reviews)
@@ -47,22 +45,18 @@ def match_query(path_json,query,top_n):
         for q_v in query_vect_corpus:
             cosine=cos(vector,q_v)
             coeffs.append(cosine)
-    coeffs_array=array(coeffs)
+    coeffs_array=np.array(coeffs)
     coeffs_argsort=coeffs_array.argsort()[::-1][:top_n]
-    results = []
-
     for i in range(top_n):
-        results.append(filenames[coeffs_argsort[i]])
-        #print i,' ',filenames[coeffs_argsort[i]],' ',coeffs[coeffs_argsort[i]]
-        # print reviews[coeffs_argsort[i]]
+        print i,' ',filenames[coeffs_argsort[i]],' ',coeffs[coeffs_argsort[i]]
+    wtf = [filenames[coeffs_argsort[i]] for i in range(top_n)]
+    return wtf
 
-    return results
-
-"""def main():
-    path_json='C:/Users/Anca/Documents/GitHub/ProjectWatson/data/'
+"""
+def main():
+    path_json='data/'
     query='I would love to read some science-fiction, science and discovery'
     top_n=10
     simpl_query=query.lower().encode('utf-8').translate(None,string.punctuation)
     match_query(path_json,simpl_query,top_n)
-
-#main()"""
+"""
--- Web_App/tf_idf.py
+++ Web_App/tf_idf.py
@@ -2,17 +2,28 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Jan 05 10:55:23 2016
-
 @author: Ana-Maria, Baoyang
 """
 import string
 import json
-import os.path
+import os
+import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
 from alchemyapi import AlchemyAPI
+from process_text import process_r, process_kw
 
 alchemyapi = AlchemyAPI()
+
+os.environ['HTTP_PROXY']="http://kuzh.polytechnique.fr:8080"
+os.environ['HTTPS_PROXY']="http://kuzh.polytechnique.fr:8080"
+
+if os.name != 'posix':
+    path = 'C:/Users/Anca/Documents/GitHub/ProjectWatson/data/'
+else:
+    path = './data/'
+
+filetype = '.json'
 
 def build_tf_idf(corpus, voc = None):
     """return a (sparse) tf-idf matrix of given corpus
@@ -25,60 +36,99 @@ def build_tf_idf(corpus, voc = None):
         vectorizer = TfidfVectorizer(vocabulary = voc, norm = 'l2',stop_words='english')
     return vectorizer
 
-def build_corpus(filenames, extract_keywords = True):
+def build_corpus(filenames, max_nb_reviews = 99, extract_keywords = True, concat_to_extract = True):
     """return a corpus and a set of keywords extracted by AlchemyAPI
     filenames is a list of string who are paths of json data files
     """
+    #Load the dictionary file, containing the words not to be taken into account   
+    dictpath= path + 'dictionary.txt'
+    dictfile= []
+    dictio = {'0': '0'}
+    shelf_vectors=[]
+    if os.path.isfile(path + 'alchemy_tentative.txt'):
+        with open(dictpath,'r') as dictionaryfile:
+            dictfile = dictionaryfile.readlines()
+        dictfile=[x[:-1] for x in dictfile]
+    
     vocabulary = []
     reviews = []
     descriptions = []
     for filename in filenames:
-        d, r, v = get_review_keywords(filename,99,extract_keywords)
-        #if (d, r, v) == ('', '', []):
-         #   continue
-        reviews.append(r.lower().encode('utf-8').translate(None,string.punctuation))
-        #
+        shelf_vect, d, r, v = get_review_keywords(filename, dictio, dictfile, max_nb_reviews, extract_keywords,
+                                                  concat_to_extract)
+        shelf_vectors.append(shelf_vect)        
+        reviews.append(process_r(r))
         descriptions.append(d)
         for i in v:
             vocabulary.append(i)
         print filename + ' processed'
-    vocabulary = set(vocabulary)
-    return descriptions, reviews, vocabulary
+    vocabulary = list(set(vocabulary))
+    
+    #Resizing the vectors inside the shelf_vectors matrix
+    for i in range(len(shelf_vectors)):
+        shelf_vectors[i].resize(len(shelf_vectors[-1]))
+    return shelf_vectors, descriptions, reviews, vocabulary
         
 
-def get_review_keywords(filename, max_nb_reviews = 99, extract_keywords = True):
+def get_review_keywords(filename, dictio = {'0' : '0'}, dictfile = [], max_nb_reviews=99, extract_keywords=True, concat_to_extract=True):
     """return a string of concatenation of
     certain number (default 99) reviews 
     and a set of keywords extracted by AlchemyAPI
     """
     # load file
     if not os.path.isfile(filename):
-        return '', '', []
+        return [], '', '', []
     with open(filename) as infile:
         data = json.load(infile)
     # extract reviews, if field not exist, None type is returned
     reviews_raw = data.get('reviews')
     description = data.get('description')
+    # shelves
+    shelves = list(set(list(set(data.get('shelves').keys())-set(dictfile)) + dictio.keys()))
+    shelf_vect = np.zeros(len(shelves))
+    
+    for shelf in shelves:
+        found = dictio.get(shelf,0)
+        if (found>0):
+            shelf_vect[found]=data.get('shelves').get(shelf,0)
+        else :
+            dictio[shelf]=len(dictio)
+            shelf_vect[dictio[shelf]]=data.get('shelves').get(shelf,0) 
+    
     if reviews_raw is None or len(reviews_raw) == 0:
-        return description, '', [] 
+        return shelf_vect, description, '', []
     # we are only interested in 'body' filed of reviews
     reviews_raw = [i.get('body') for i in reviews_raw]
+    # remove duplicate reviews
+    reviews_raw = list(set(reviews_raw))
     # determine how many reviews are going to used
     nb_reviews = min(max_nb_reviews, len(reviews_raw))
     # concatenation of reviews into a single string splited by return
-    reviews = '\n'.join(reviews_raw[:nb_reviews])
+    if concat_to_extract:
+        reviews = ['\n'.join(reviews_raw[:nb_reviews])]
+    else:
+        reviews = reviews_raw[:nb_reviews]
     if not extract_keywords:
-        return description, reviews, [] 
-    # extract entities
-    response_entities = alchemyapi.entities("text", reviews)  
-    entities = [i.get('text') for i in response_entities.get('entities')]
-    # extract keywords
-    response_keywords = alchemyapi.keywords("text", reviews)
-    if response_keywords is None:
-        return description, reviews, []
-    # we do not consider entities as keywords
-    keywords = [i.get('text') for i in response_keywords.get('keywords')]
-    return description, reviews, list(set(keywords) - set(entities))
+        return shelf_vect, description, '\n'.join(reviews), []
+    keywords = []
+    entities = []
+    print 'Number of reviews:',len(reviews)
+    for review in reviews:
+        # extract entities
+        response_entities = alchemyapi.entities("text", review)
+        if response_entities is not None and response_entities.get('entities') is not None:
+            entities.extend([i.get('text') for i in response_entities.get('entities')])
+        # extract keywords
+        response_keywords = alchemyapi.keywords("text", review)
+        if response_keywords is not None and response_keywords.get('keywords') is not None:
+            l=[]
+            for i in response_keywords.get('keywords'):
+                l=process_kw(i.get('text'))+l
+            keywords=l+keywords
+    #Processing the text of reviews - removing the upper case letters,
+    # And the punctuation except for the '
+    reviews=[process_r(review) for review in reviews]
+    return shelf_vect, description, '\n'.join(reviews), list(set(keywords) - set(entities))
     
 def similarities(tf_idf):
     """return a (symmetric) matrix
@@ -87,38 +137,9 @@ def similarities(tf_idf):
     dist = linear_kernel(tf_idf)
     return dist
 
-""" 
-def main():
-    # if no argument is given, use default setting
-    if len(sys.argv) < 2:
-        filenames = ['../data/1.json', '../data/35.json','../data/37.json','../data/101.json','../data/41804.json','../data/120725.json', '../data/77366.json', '../data/9520360.json', '../data/14406312.json', '../data/15872.json']
-    # otherwise take system argument as file paths
-    else:
-        filenames = sys.argv[1:]
-    descriptions, corpus, vocabulary = build_corpus(filenames,False)
-    print 'Vocabulary',vocabulary
-    # calculate tf-idf matrix based on corpus and vocabulary
-#    tf_idf = build_tf_idf(corpus, vocabulary)
-    # calculate tf-idf matrix only with corpus
-    vect_corpus = build_tf_idf(corpus)
-    tf_idf_benchmark=vect_corpus.fit_transform(corpus).toarray()
-    # calculate similarity matrices
-#    dist = similarities(tf_idf)
-    dist_benchmark = similarities(tf_idf_benchmark)
- #   print 'Distance entre les reviews, avec Alchemy: '
-  #  print dist
-  #  print 'Distance entre les reviews, sans Alchemy: '
-    print dist_benchmark
- #   tf_idf_descr=build_tf_idf(descriptions)
-#    dist_descr = similarities(tf_idf_descr)   
-   # print 'Distance entre les descriptions, sans Alchemy: '
-  #  print dist_descr
-"""
+'''def main():
+   filenames = ['../data/1.json', '../data/35.json','../data/37.json','../data/101.json','../data/41804.json','../data/120725.json', '../data/77366.json', '../data/9520360.json',  '../data/15872.json']
+   _, d, r, voc=get_review_keywords(filenames[0],concat_to_extract=False)
+   print voc
 
-def main():
-   # d,r,v=get_review_keywords('../data/1.json',99,False)
-   #filenames = ['../data/1.json', '../data/35.json','../data/37.json','../data/101.json','../data/41804.json','../data/120725.json', '../data/77366.json', '../data/9520360.json',  '../data/15872.json']
-   filenames = ['../data/1.json', '../data/35.json','../data/15872.json']   
-   description,reviews,vocabulary=build_corpus(filenames,True)
-
-main()
+main()'''
